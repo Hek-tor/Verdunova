e3374f1f558011675ba021bbc041f38d4c9f593d
Hek-tor <EmilioCmDz@gmail.com>
2024-03-15 19:46:15 -0600

    Add modals for selected products.

--- js/controllers/itemsController/itemsView.js
+++ js/controllers/itemsController/itemsView.js
@@ -15,7 +15,38 @@ export class ItemsView extends ViewForController {
     }
 
     showProductSelected(event) {
-        let productSelected = event.detail.ProductView.product;
-
+        let product = event.detail.ProductView.product;
+        Swal.fire({
+            title: `¿Agregar ${(product.name).toLowerCase()}?`,
+            imageUrl: `${product.imageURL}`,
+            imageWidth: 320,
+            imageHeight: 180,
+            imageAlt: `Producto: ${product.name}`,
+            showConfirmButton: true,
+            confirmButtonColor: "#298779",
+            confirmButtonText: "Agregar",
+            showCloseButton: true,
+            input: "text",
+            inputPlaceholder: "Ejemplo: 1 kilo pero pequeños",
+            customClass: {
+                popup: 'productModal',
+                input: 'inputModal',
+                title: 'modalTittle',
+                image: 'modalImage',
+                closeButton: 'closeButtonModal',
+                confirmButton: 'confirmButtonModal',
+            },
+            showClass: {
+                popup: `animate__animated animate__fadeInDown animate__faster`
+            },
+            hideClass: {
+                popup: `animate__animated animate__bounceOut animate__faster`
+            },
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //Usuario agrego algo
+            }
+        });
     }
 }
+
