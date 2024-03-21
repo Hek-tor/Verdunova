dbe7f2fd69d6b141e1286d39dd2c3aefbc9e9b30
Hek-tor <EmilioCmDz@gmail.com>
2024-03-20 23:25:02 -0600

    Add display button effect

--- js/controllers/cartController/cartView.js
+++ js/controllers/cartController/cartView.js
@@ -8,6 +8,18 @@ export class CartView extends ViewForController {
         this.iconButton = img(this.cartButton, { className: 'cartIcon' });
         this.iconButton.src = '../assets/icons/cart.webp';
         this.iconButton.setAttribute('alt', 'Botón para abrir el carrito de compras');
+        this.activeButtonEvent();
+    }
+
+    activeButtonEvent() {
+        window.onscroll = function () {
+            let scroll = document.documentElement.scrollTop || document.body.scrollTop;
+            if (scroll > 500) {
+                this.cartButton.style.display = "block";
+            } else {
+                this.cartButton.style.display = "none";
+            }
+        }.bind(this);
     }
 
     cartSection() {
