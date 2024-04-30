461f3a25fa5d40b00768ab8ad99a9a6aab5945ad
Hek-tor <EmilioCmDz@gmail.com>
2024-04-29 21:56:36 -0600

    Bug fixed to save cache in js files

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{ttf,webp,svg,png,css,html,json,md}'
+		'**/*.{ttf,webp,svg,png,css,html,js,md}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
